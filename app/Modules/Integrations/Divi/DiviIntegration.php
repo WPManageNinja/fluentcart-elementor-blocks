3fe4de930370c549cca9b6e2510564dd116fdad5
Nuhel <nuheljft@gmail.com>
2026-02-09 10:05:20 +0600

    Wip.

--- app/Modules/Integrations/Divi/DiviIntegration.php
+++ app/Modules/Integrations/Divi/DiviIntegration.php
@@ -11,13 +11,37 @@ use FluentCartElementorBlocks\App\Modules\Integrations\Divi\Modules\Legacy\Produ
 
 class DiviIntegration
 {
+    private static $moduleSlugs = [
+        'fceb_mini_cart',
+        'fceb_add_to_cart',
+        'fceb_buy_now',
+        'fceb_product_categories_list',
+        'fceb_product_carousel',
+        'fceb_checkout',
+    ];
+
     public function register()
     {
+        // Register module slugs for Divi 5's lazy shortcode loading on frontend
+        \add_filter('et_builder_3rd_party_module_slugs', [$this, 'registerModuleSlugs']);
+
+        // Register the actual module instances when the builder framework is ready
         \add_action('et_builder_ready', [$this, 'registerModules']);
+    }
+
+    public function registerModuleSlugs(array $slugs): array
+    {
+        return array_merge($slugs, self::$moduleSlugs);
     }
 
     public function registerModules()
     {
+        static $registered = false;
+        if ($registered) {
+            return;
+        }
+        $registered = true;
+
         new MiniCartModule();
         new AddToCartModule();
         new BuyNowModule();
